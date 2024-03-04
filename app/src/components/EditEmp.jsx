b296a721e643a90055c3d248c8eed676303d20c0
Aishwarya Arun <jj4245888@gmail.com>
2024-03-04 12:50:45 +0530

    error middleware edited

--- app/src/components/EditEmp.jsx
+++ app/src/components/EditEmp.jsx
@@ -87,6 +87,8 @@ export default function Component() {
                   type="text"
                   autoComplete="name"
                   required
+                  pattern="[A-Za-z0-9]+"
+                  title="Only letters and numbers are allowed"
                   {...register('name')}
                   className="p-3 bg-blue-200 block w-full rounded-md border-0 py-1.5 text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
--- app/src/pages/AddEmployee.jsx
+++ app/src/pages/AddEmployee.jsx
@@ -63,6 +63,8 @@ const AddEmployee = () => {
                   type="number"
                   autoComplete="id"
                   required
+                  pattern="[A-Za-z0-9]+"
+                  title="Only letters and numbers are allowed"
                   {...register('id')}
                   className="p-3 bg-gray-200 block w-full rounded-md border-0 py-1.5 text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
